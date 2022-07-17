7bf144dff80c49fd7e99b4430a01ff1d6d3cc27f
Blazing42 <Blazing42@users.noreply.github.com>
2022-07-17 17:19:57 +0100

    fixed save file

--- Assets/Scripts/TimerandKillSave.cs
+++ Assets/Scripts/TimerandKillSave.cs
@@ -6,6 +6,20 @@ public class TimerandKillSave : MonoBehaviour
 {
     public string timer;
     public int kills;
+    public static TimerandKillSave SaveInstance { get; private set; }
+
+    private void Awake()
+    {
+        // If there is an instance
+        if (SaveInstance != null && SaveInstance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            SaveInstance = this;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
