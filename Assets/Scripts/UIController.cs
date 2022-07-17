b2bd4e47f8e6c4f8220912a9d97ec5b457ce65bb
Blazing42 <Blazing42@users.noreply.github.com>
2022-07-17 09:43:49 +0100

    more UI

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -13,15 +13,32 @@ public class UIController : MonoBehaviour
     [SerializeField] TextMeshProUGUI attacktype;
 
     private float startTime;
-    bool timeGoing;
+    int killCount = 0;
+    int livesLeft = 4;
     EnemySpawner enemySpawner;
-    
+
+    public static UIController UIControllerInstance { get; private set; }
+
+    private void Awake()
+    {
+        // If there is an instance
+        if (UIControllerInstance != null && UIControllerInstance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            UIControllerInstance = this;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         enemySpawner = GameObject.Find("EnemySpawner").GetComponent<EnemySpawner>();
         timer.text = "00:00";
+        enemiesKilled.text = "Kills : " + killCount;
+        lives.text = "Lives : " + livesLeft;
         startTime = Time.time;
     }
 
@@ -41,5 +58,20 @@ public class UIController : MonoBehaviour
         }
     }
 
+    public void LoseLife()
+    {
+        livesLeft--;
+        lives.text = "Lives : " + livesLeft;
+        if(livesLeft <= 0)
+        {
+            //change to lose screen etc
+        }
+    }
+
+    public void TickUpKillcount()
+    {
+        killCount++;
+        enemiesKilled.text = "Kills : " + killCount;
+    }
 
 }
